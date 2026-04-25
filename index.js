2b59e820d0ae69994b6a084af58a2a3295dff18c
Thom Manning <84347286+manni155@users.noreply.github.com>
2026-04-25 10:10:07 +0000

    complete draft 1

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Add JavaScript below
+const openButton = document.querySelector("#open-modal")
+const closeButton = document.querySelector("#close-modal")
+const modal = document.querySelector("#modal")
+
+openButton.addEventListener("click", () => {
+    modal.showModal()
+})
+
+closeButton.addEventListener("click", () => {
+    modal.close()
+})
---
+++ mail.php
@@ -0,0 +1,12 @@
+<?php
+
+$name = $_POST['userName'];
+$email = $_POST['userEmail'];
+$message = $_POST['message'];
+$mailHeader = "From:".$name."<".$email.">\r\n";
+$recipient = "manni155@msu.edu";
+$subject = "New Form Submission";
+
+mail($recipient, $subject, $message, $mailHeader)
+or die("Error reported.");
+?>
